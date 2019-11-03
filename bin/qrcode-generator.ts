75f277ced098f4d02aef8fef04545f63a15a123d
HeRoMo <hero@asterisk-works.jp>
2019-11-03 11:08:02 +0900

    fix eslint warnings

--- bin/qrcode-generator.ts
+++ bin/qrcode-generator.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
+import { App } from '@aws-cdk/core';
 import { QrcodeGeneratorStack } from '../lib/qrcode-generator-stack';
 
-const app = new cdk.App();
+const app = new App();
 new QrcodeGeneratorStack(app, 'QrcodeGeneratorStack');
--- lib/qrcode-generator-stack.ts
+++ lib/qrcode-generator-stack.ts
@@ -1,7 +1,7 @@
-import cdk = require('@aws-cdk/core');
+import { Construct, Stack, StackProps } from '@aws-cdk/core';
 
-export class QrcodeGeneratorStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class QrcodeGeneratorStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
