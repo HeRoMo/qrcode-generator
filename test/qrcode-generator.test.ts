6d8fdfd7fc668f307a8a0364e97e4244ab49d330
HeRoMo <hero@asterisk-works.jp>
2019-11-07 19:27:02 +0900

    add test

--- test/qrcode-generator.test.ts
+++ test/qrcode-generator.test.ts
@@ -1,13 +1,35 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import { App } from '@aws-cdk/core';
+import '@aws-cdk/assert/jest';
+import { SynthUtils } from '@aws-cdk/assert';
+import { App, Stack } from '@aws-cdk/core';
 import { QrcodeGeneratorStack } from '@lib/qrcode-generator-stack';
 
-test('Empty Stack', () => {
-  const app = new App();
-  // WHEN
-  const stack = new QrcodeGeneratorStack(app, 'MyTestStack');
-  // THEN
-  expectCDK(stack).to(matchTemplate({
-    Resources: {},
-  }, MatchStyle.EXACT));
+describe('Snapshot Test', () => {
+  it('match snapshot', () => {
+    const app = new App();
+    const stack = new QrcodeGeneratorStack(app, 'MyTestStack');
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
+});
+
+describe('Contain specific resources', () => {
+  let stack: Stack;
+  beforeEach(() => {
+    const app = new App();
+    stack = new QrcodeGeneratorStack(app, 'MyTestStack');
+  });
+  it('have Lambda Layer', () => {
+    expect(stack).toHaveResource('AWS::Lambda::LayerVersion', {
+      CompatibleRuntimes: ['nodejs10.x'],
+    });
+  });
+  it('have Lambda Function', () => {
+    expect(stack).toHaveResource('AWS::Lambda::Function', {
+      Runtime: 'nodejs10.x',
+    });
+  });
+  it('have ApiGateway', () => {
+    expect(stack).toHaveResource('AWS::ApiGateway::RestApi', {
+      Name: 'qrcode',
+    });
+  });
 });
